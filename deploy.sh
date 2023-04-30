a199da360a538dcfabae2e1a3229578fbb51144d
niyas <withniyaz@gmail.com>
2023-04-30 12:01:08 +0100

    xcard sms implementation

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ cd ~
 
 echo "✅ ✅ ✅  SSH Connection Established ✅ ✅ ✅"
 
-ssh -o PasswordAuthentication=yes ubuntu@45.118.161.254 'sudo pm2 delete --silent xcard && cd /var/www/ && sudo rm -rf xcard && sudo git clone --single-branch --branch main https://github.com/withniyaz/xcard-API.git xcard && sudo chmod -R 777 xcard/ && cd xcard && npm i && pm2 start --name=xcard npm -- start -i max'
+ssh -o PasswordAuthentication=yes ubuntu@45.118.161.254 './deploy.sh'
 
 echo "🚀🚀🚀  Xcard Deployed  🚀🚀🚀 "
