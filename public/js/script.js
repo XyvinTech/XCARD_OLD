2493c5233699e3e669f15775e7c633b6a3c8f6ee
niyas <withniyaz@gmail.com>
2023-06-02 23:49:50 +0100

    Xcard Test Number Method

--- public/js/script.js
+++ public/js/script.js
@@ -99,7 +99,7 @@ const personData = {
   company: data?.profile?.companyName,
   position: data?.profile?.designation,
   phone: data?.contact?.contacts[0]?.value,
-  websites: data?.website?.websites,
+  websites: [{ link: `${window.location.href}` }, ...data?.website?.websites],
   address: `${data?.contact?.contacts[3]?.value}, ${data?.contact?.contacts[3]?.street}, ${data?.contact?.contacts[3]?.pincode}`,
   whatsapp: data?.contact?.contacts?.find((item) => item.type === "whatsapp")
     .value,
