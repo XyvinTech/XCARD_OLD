bf76118271d66fd7498eb05fa77272459b325d44
Monarch <vichukartha@gmail.com>
2023-08-13 08:09:05 +0530

    spinner bg

--- public/js/script.js
+++ public/js/script.js
@@ -31,7 +31,7 @@ function showFullScreenSpinner() {
   document.body.appendChild(spinner);
 
   // Optional: You can add additional styling to the spinner element
-  spinner.style.background = 'rgba(0, 0, 0, 0.5)';
+  spinner.style.background = 'rgba(255, 255, 255, 1)';
   spinner.style.display = 'flex';
   spinner.style.justifyContent = 'center';
   spinner.style.alignItems = 'center';
